2fa63659e60289acb82fff8fe34596c33d1cabfc
JMAbel-hub <72168297+JMAbel-hub@users.noreply.github.com>
2022-04-22 03:55:35 -0400

    Finished stage five and added interactive elements

---
+++ javadiscover/helloworld/scripts/default.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    // Zoom plugin
+    $('.zoom').wrap('<span style="display:inline-block"></span>')
+    .css('display', 'block')
+    .parent()
+    .zoom({ 
+        // Magnify 150% and is enabled on click 
+        magnify: 1.5,
+        on: 'click'
+    });
+    // Slider plugin
+    $("#range").ionRangeSlider({
+        type: "single",
+        min: 1,
+        max: 5,
+        from: 1,
+        grid: true,
+        from_fixed: true
+    });
+});
---
+++ javadiscover/scripts/default.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // implementation of the accordion widget
+    $('#accordion').accordion({
+        active: 0,
+        collapsible: true
+    });
+});
