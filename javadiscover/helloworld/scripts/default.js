83651ad50638350637933f1e399bb9bf7a9f58b0
JMAbel-hub <72168297+JMAbel-hub@users.noreply.github.com>
2022-04-28 20:58:39 -0400

    Added Ajax functionality

--- javadiscover/helloworld/scripts/default.js
+++ javadiscover/helloworld/scripts/default.js
@@ -17,4 +17,34 @@ $(document).ready(function() {
         grid: true,
         from_fixed: true
     });
+    // Gets the default content for the page from a json
+    $.ajax({ 
+        type: "get",
+        url: "json_files/default.json",
+        beforeSend: function() {
+            $("#content").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#content").html("");
+            $.each(data, function() {
+                $.each(this, function(key, value) {
+                    $("#content").append(
+                        "<h3>" + value.section + "</h3>" + 
+                        "<p>" + value.content + "</p>" +
+                        "<figure class='center'>" +
+                        "<a href=" + value.picture + ">" +
+                        "<img class='zoom' src = '" + value.picture + "' alt=example of a default and full constructor>" +
+                        "</a>" + 
+                        "<figcaption>" + value.caption + "</figcaption>" + 
+                        "</figure>" + "<hr>"
+                    );
+                });
+            });
+        }
+    });
 });
--- javadiscover/objectoriented/scripts/default.js
+++ javadiscover/objectoriented/scripts/default.js
@@ -17,4 +17,34 @@ $(document).ready(function() {
         grid: true,
         from_fixed: true
     });
+    $("#solution_button").click(function(){
+        $.ajax({ 
+            type: "get",
+            url: "json_files/solution.json",
+            beforeSend: function() {
+                $("#solution").html("Loading...");
+            },
+            timeout: 10000,
+            error: function(xhr, status, error) {
+                alert("Error: " + xhr.status + " - " + error);
+            },
+            dataType: "json",
+            success: function(data) {
+                $("#solution").html("");
+                $.each(data, function() {
+                    $.each(this, function(key, value) {
+                        $("#solution").append(
+                            "<figure class='center'>" +
+                            "<a href=" + value.picture + ">" +
+                            "<img class='zoom' src = '" + value.picture + "' alt=example of a default and full constructor>" +
+                            "</a>" + 
+                            "<figcaption>" + value.caption + "</figcaption>" + 
+                            "</figure>" +
+                            "<p>" + value.explanation + "</p>"
+                        );
+                    });
+                });
+            }
+        });
+    });
 });
